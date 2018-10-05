c81ba16d6084c8ddb801653c6c58760d07abbbd8
alexdejko <dag@brest.beltelecom.by>
2018-10-05 15:23:02 +0300

    Calculator

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 class SmartCalculator {
   constructor(initialValue) {
+    this.num = initialValue;
     // your implementation
   }
 
   add(number) {
+    this.num += "+" + number;
+    return this;
     // your implementation
   }
   
   subtract(number) {
+    this.num += "-" + number;
+    return this;
     // your implementation
   }
 
   multiply(number) {
+    this.num += "*" + number;
+    return this;
     // your implementation
   }
 
   devide(number) {
+    this.num += "/" + number;
+    return this;
     // your implementation
   }
 
   pow(number) {
+    this.num += "**" + number;
+    return this;
     // your implementation
+  }
+  toString(){
+    return eval(this.num);
   }
 }
 
